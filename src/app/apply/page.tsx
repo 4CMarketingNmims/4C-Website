3796eb3a08474541bf79d9c2831640c8df66abdf
4CMarketingNmims <4c.nmims@gmail.com>
2026-05-30 10:37:37 +0530

    Create apply page.tsx

---
+++ src/app/apply/page.tsx
@@ -0,0 +1,13 @@
+export default function ApplyPage() {
+  return (
+    <div className="w-full h-screen">
+      <iframe
+        src="https://tally.so/r/yPorpB"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        title="4C Core Applications"
+      />
+    </div>
+  );
+}
