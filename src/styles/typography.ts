0085b62844a97320dcc06a7cd7f1c3620a1f4ed1
4CMarketingNmims <4c.nmims@gmail.com>
2026-07-07 15:10:49 +0530

    Create typography.ts

---
+++ src/styles/typography.ts
@@ -0,0 +1,11 @@
+export const typography = {
+  hero: "text-7xl md:text-8xl lg:text-9xl",
+
+  heading: "text-5xl md:text-6xl",
+
+  subheading: "text-2xl md:text-3xl",
+
+  body: "text-lg md:text-xl",
+
+  small: "text-sm tracking-[0.2em] uppercase",
+};
