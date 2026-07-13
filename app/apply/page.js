f489a44338f3e9ee5e8db2f9736cd0db11587c9d
4CMarketingNmims <4c.nmims@gmail.com>
2026-07-13 22:39:24 +0530

    Create page.js

---
+++ app/apply/page.js
@@ -0,0 +1,11 @@
+import RecruitmentLanding from "@/components/RecruitmentLanding";
+
+export const metadata = {
+  title: "Executive Recruitment | 4C NMIMS",
+  description:
+    "Join the Official Marketing Cell of NMIMS MPSTME. Executive Recruitment 2026–27 is now open.",
+};
+
+export default function ApplyPage() {
+  return <RecruitmentLanding />;
+}
