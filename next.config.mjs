cb20dd929c4d7281607ac68105ba5e23079bfef9
4CMarketingNmims <4c.nmims@gmail.com>
2026-08-13 23:51:48 +0530

    Add redirect for '/wnr' to external URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/wnr',
+        destination:
+          'https://unstop.com/college-fests/wings-and-roots-50-narsee-monjee-institute-of-management-studies-nmims-mumbai-498354',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
